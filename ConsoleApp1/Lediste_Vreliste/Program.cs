a0b48a51bb4b9465bed90e738975a3fa3805c380
programer10 <programer10@ucione.local>
2019-02-21 18:54:51 +0100

    Uvodni programi iz zbirke

---
+++ ConsoleApp1/Lediste_Vreliste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lediste_Vreliste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lediste = 0;
+            int vreliste = 100;
+
+            Console.WriteLine("Lediste vode je " + lediste + ", a vreliste je " + vreliste + " Celzijevih stupnjeva");
+            Console.ReadKey();
+        }
+    }
+}
