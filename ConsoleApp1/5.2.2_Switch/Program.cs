ce735c2d77f7080c3d1fbc81c375d208d5f86389
Dalovric <davor.lovric85@gmail.com>
2019-02-25 17:22:05 +0100

    Switch case primjer dodan

---
+++ ConsoleApp1/5.2.2_Switch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._2_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Primjer kalkulator
+
+            float a = 0, b = 0;
+            string operacija = "";
+
+            Console.WriteLine("Unesi prvi prirodan broj: ");
+            a = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi drugi prirodan broj: ");
+            b = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesi operaciju + - * /");
+            operacija = Console.ReadLine();
+
+            switch(operacija)
+            {
+                case "+":
+                case "plus":
+                    Console.WriteLine("Zbroj je {0} + {1} = {2}",a,b, a + b);
+                    break;
+                case "-":
+                    Console.WriteLine("Razlika je {0} - {1} = {2}", a, b, a - b);
+                    break;
+                case "*":
+                case "x":
+                    Console.WriteLine("Umnozak je {0} * {1} = {2}", a, b, a * b);
+                    break;
+                case "/":
+                    Console.WriteLine("Kvocijent je {0} / {1} = {2}", a, b, a / b);
+                    break;
+                default:
+                    Console.WriteLine("Nepoznata operacija");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
