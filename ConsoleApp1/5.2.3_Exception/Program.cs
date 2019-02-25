59063b99a545ee88b7b19755ee4d410cfb2d1730
Dalovric <davor.lovric85@gmail.com>
2019-02-25 17:38:11 +0100

    dodan exception

--- ConsoleApp1/5.2.3_Exception/Program.cs
+++ ConsoleApp1/5.2.3_Exception/Program.cs
@@ -10,7 +10,31 @@ namespace _5._2._3_Exception
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.Write("Unesi cijeli broj: ");
+                int a = int.Parse(Console.ReadLine());
+                if (a > 0)
+                {
+                    Console.WriteLine("Uneseni broj je pozitivan.");
+                }
+                else if (a < 0)
+                {
+                    Console.WriteLine("Uneseni broj je negativan.");
+                }
+                else
+                {
+                    Console.WriteLine("Uneseni broj je jednak nuli.");
+                }
+            }
+            catch (FormatException fe)
+
+            {
+                Console.WriteLine("Opis greske: " + fe.Message);
+            }
+            Console.ReadKey();
 
         }
     }
 }
+
