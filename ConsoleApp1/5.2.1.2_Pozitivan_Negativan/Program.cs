f233847be60149b9fb2d80b7cc4e2326097c20e1
Dalovric <davor.lovric85@gmail.com>
2019-02-25 16:50:01 +0100

    Pozitivan negativan

---
+++ ConsoleApp1/5.2.1.2_Pozitivan_Negativan/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._1._2_Pozitivan_Negativan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi cijeli broj: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 0)
+            {
+                Console.WriteLine("Uneseni broj je pozitivan.");
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("Uneseni broj je negativan.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je jednak nuli.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5_2_1_veci_od_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_1_veci_od_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi prirodan broj: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10)
+            {
+                Console.WriteLine("Uneseni broj je veci od 10.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj NIJE veci od 10.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
