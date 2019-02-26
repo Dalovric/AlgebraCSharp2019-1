5aa45d6d0582a1e1534b93ae6d8694fd542133e1
Davor Lovric <dalovric@hotmail.com>
2019-02-26 16:17:26 +0100

    Dodani veliki broj, i x-znamenkasti zadaci

---
+++ ConsoleApp1/5.2.3_Velik_broj/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._3_Velik_broj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi jedan broj: ");
+            double a = double.Parse(Console.ReadLine());
+
+            if (a >= 100)
+            {
+                Console.WriteLine("Broj je velik.");
+
+            }
+            else
+            {
+                Console.WriteLine("Broj nije velik.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5.2.4_x_znamenkasti/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._2._4_x_znamenkasti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi jedan cijeli broj: ");
+            int i = int.Parse(Console.ReadLine());
+
+            if (i < 0) i = -i;
+            if (i < 10)
+            {
+                Console.WriteLine("Uneseni broj je jednoznamenkast");
+            }
+            else if (i < 100)
+            {
+                Console.WriteLine("Uneseni broj je dvoznamenkast");
+            }
+            else if (i < 1000)
+            {
+                Console.WriteLine("Uneseni broj je troznamenkast");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je viseznamenkast");
+            }
+            Console.ReadKey();
+        }
+    }
+}
