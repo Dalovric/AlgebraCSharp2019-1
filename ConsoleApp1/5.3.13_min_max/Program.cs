c7196caf3f1572451517245fc2d53c2cb9248c4b
Davor Lovric <dalovric@hotmail.com>
2019-02-27 18:31:00 +0100

    dodani 5.3.13, 5.3.15, 5.3.17 zadaci

---
+++ ConsoleApp1/5.3.13_min_max/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._13_min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for (int i = 1; i <= 10; i++)
+
+            {
+                Console.WriteLine("Unesi broj");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n < min)
+                {
+                    min = n;
+                }
+                if (n > max)
+                {
+                    max = n;
+                }
+            }
+            Console.Write("Najmanji = {0}, najveci = {1}", min, max);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/5.3.17_Potencije/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._17_Potencije
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            for (int i = 1; i <= 10; i++)
+            {
+                n = 1;
+                for (int j = 1; j <= i; j++)
+                {
+                    n *= 2;
+
+                }
+                Console.WriteLine("2 na {0} = {1}", i, n);
+            }
+            Console.ReadKey();
+        }
+    }
+}
