a63ee95e82d62c7fbecf2a1cc3a8846820906c6d
Davor Lovric <dalovric@hotmail.com>
2019-02-27 17:00:54 +0100

    dodan exception na 5.3.5.

---
+++ ConsoleApp1/5.3.4_faktorijel/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._4_faktorijel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Molim unesi cijeli pozitivni broj: ");
+            // 5! = 120 (1*2*3*4*5)
+            try {
+                int a = int.Parse(Console.ReadLine());
+                if (a <= 0)
+                {
+                    throw new NegativeNumberException("Negativni broj!");
+                }
+                if (a >= 20)
+                {
+                    throw new Exception("Broj je veci od 20!");
+                }
+                int faktorijel = 1;
+                for (int i = a; i > 0; i--)
+                {
+                    // Console.WriteLine(i);
+                    faktorijel *= i;
+                }
+                Console.Write("{0}", faktorijel);
+            }
+
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine("Dozvoljeni brojevi su pozitivni", ex.ToString());
+            }
+            catch (VeciOdDvadeset ex)
+            {
+                Console.WriteLine("Dozvoljeni brojevi su u intervalu od [1,19]", ex.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.ReadLine();
+            }
+        }
+    public class NegativeNumberException: Exception
+    {
+        public NegativeNumberException()
+        {
+
+        }
+    public class VeciOdDvadeset : Exception
+        {
+            public NegativeNumberException()
+            {
+
+            }
+        }
+        public class VeciOdDvadeset (string message)
+        {
+            public NegativeNumberException()
+            {
+
+            }
+        }
+
+    } 
---
+++ ConsoleApp1/5.3.5_djeljivi_s_3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._3._5_djeljivi_s_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Unesi prvu granicu intervala");
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Unesi drugu granicu intervala");
+                int b = int.Parse(Console.ReadLine());
+
+                if (b < a)
+                {
+                    throw new Exception("Neispravan interval");
+                }
+
+                for (int i = a; i <= b; i++)
+                {
+                    if (i % 3 == 0)
+                        Console.Write("{0} ", i);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
