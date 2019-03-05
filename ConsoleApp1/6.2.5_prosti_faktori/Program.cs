2b57b358518752f75488ab6e9a13c672bda5f595
Davor Lovric <dalovric@hotmail.com>
2019-03-05 18:48:47 +0100

    dodani 7.2 primjeri

---
+++ ConsoleApp1/6.2.5_prosti_faktori/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2._5_prosti_faktori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = int.Parse(Console.ReadLine());
+
+            List<int> prosti = new List<int>();
+            int faktor = 2;
+            while(faktor<=broj)
+            {
+                if (broj % faktor == 0)
+                {
+                    prosti.Add(faktor);
+                    broj = broj / faktor;
+                }
+                else
+                {
+                    faktor++;
+
+               }
+                
+            }
+            foreach (int item in prosti)
+            {
+                Console.Write(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
