f3cfd90bfeb712ca100c5be346a2c7c5111c4d56
Davor85 <davor.lovric85@gmail.com>
2019-03-09 14:35:52 +0100

    dodana zadaca 11.3

---
+++ ConsoleApp1/Zadaca_11.3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaca_11._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long skolarina = 1200;
+
+            List<Ucenik> listaUcenika = new List<Ucenik>();
+
+            Ucenik ucenik1 = new Ucenik("Ivan", "Ivic");
+
+            Ucenik ucenik2 = new Ucenik("Marko", "Maric", 12345678901);
+
+            Ucenik ucenik3 = new Ucenik("Iva", "Ivic", 12345678901, 4500 );
+
+            listaUcenika.Add(ucenik1);
+            listaUcenika.Add(ucenik2);
+            listaUcenika.Add(ucenik3);
+
+            foreach (Ucenik uc in listaUcenika)
+
+            {
+                //Console.WriteLine("Ime: {0} Prezime: {1}, racun:{2}", uc.Ime, uc.Prezime, uc.Racun);
+                Console.WriteLine(uc.ToString());
+                uc.Racun -= skolarina; 
+            }
+
+            Console.WriteLine("");
+
+            foreach (Ucenik uc in listaUcenika)
+
+            {
+                Console.WriteLine("Ime: {0} Prezime: {1}, racun:{2}", uc.Ime, uc.Prezime, uc.Racun);
+                
+            }
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ ConsoleApp1/Zadaca_11.3/Ucenik.cs
@@ -0,0 +1,43 @@
+﻿namespace Zadaca_11._3
+{
+    internal class Ucenik
+    {
+        private string ime = "";
+        private string prezime = "";
+        private long oib = 0;
+        public string telefon = "";
+        private long racun = 0;
+
+        public string Ime { get => ime; }
+
+        public string Prezime { get => prezime;}
+
+        public long Oib { set => oib = value; }
+
+        public long Racun { get => racun; set => racun = value; }
+
+        public override string ToString()
+        {
+            string poruka = string.Format("pozdrav {0} {1} {2}", ime, prezime, racun);
+            return poruka;
+        }
+
+        public Ucenik(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.racun = 3000;
+        }
+
+        public Ucenik(string ime, string prezime, long oib) : this(ime, prezime)
+        {
+            this.oib = oib;
+            this.racun = 4000;
+        }
+
+        public Ucenik(string ime, string prezime, long oib, long racun) : this(ime, prezime, oib)
+        {
+            this.racun = racun;
+        }
+    }
+}
