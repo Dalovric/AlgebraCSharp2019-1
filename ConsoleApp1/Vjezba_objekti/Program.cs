37d112c4cad7175356523340163efd56f6be1c19
Davor85 <davor.lovric85@gmail.com>
2019-03-12 15:04:13 +0100

    Dodan static object primjer

---
+++ ConsoleApp1/Vjezba_objekti/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vjezba_objekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            long skolarina = 1200;
+
+            List<Ucenik> listaUcenika = new List<Ucenik>();
+
+            Ucenik ucenik1 = new Ucenik("Marko", "Maric");
+
+            Ucenik ucenik2 = new Ucenik("Jerko", "Kovac", 12345678901);
+
+            Ucenik ucenik3 = new Ucenik("Mario", "Mihic", 12345678901, 4500);
+
+            listaUcenika.Add(ucenik1);
+            listaUcenika.Add(ucenik2);
+            listaUcenika.Add(ucenik3);
+
+            foreach (Ucenik uc in listaUcenika)
+            {
+                //Console.WriteLine("Ime {0}, prezime {1}, racun {3}", uc.Ime, uc.Prezime, uc.Racun);
+                Console.WriteLine(uc.ToString());
+                uc.Racun -= skolarina;
+            }
+
+            Console.WriteLine("");
+
+
+            foreach (Ucenik uc in listaUcenika)
+            {
+                Console.WriteLine("Ime {0}, prezime {1}, racun {2}", uc.Ime, uc.Prezime, uc.Racun);
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Vjezba_objekti/Ucenik.cs
@@ -0,0 +1,47 @@
+﻿namespace Vjezba_objekti
+{
+    internal class Ucenik
+    {
+        private string ime = "";
+        private string prezime = "";
+        private long oib = 0;
+        private string telefon = "";
+        private long racun = 0;
+
+        
+
+        public string Ime { get => ime; }
+
+        public string Prezime { get => prezime; }
+
+        public long Oib {set => oib = value; }
+
+        public long Racun { get => racun; set => racun = value; }
+
+        public override string ToString()
+        {
+            string poruka = string.Format("Pozdrav {0}, {1}, {2}", ime, prezime, racun);
+            return poruka;
+        }
+
+        public Ucenik(string ime, string prezime)
+        {
+            this.ime = ime;
+            this.prezime = prezime;
+            this.racun = 3000;
+        }
+
+        public Ucenik(string ime, string prezime, long oib) : this(ime, prezime)
+        {
+            this.oib = oib;
+            this.racun = 4000;
+
+        }
+
+        public Ucenik(string ime, string prezime, long oib, long racun) : this(ime, prezime, oib)
+        {
+            this.racun = racun;
+        }
+    }
+
+}
