60a38d990a837670960f6f2a937c7ef02d54b57e
programer10 <programer10@ucione.local>
2019-03-20 18:53:18 +0100

    dodan fakultet baza

---
+++ ConsoleApp1/SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿/* blok komentar
+-- linijski komentar
+CREATE DATABASE Upisi
+GO
+*/
+USE Upisi;
+CREATE TABLE #privremena(
+id INT,
+ime NCHAR(50),
+prezime VARCHAR(50),
+mjesto_stanovanja NCHAR(25)
+);
+
+INSERT INTO [dbo].[#privremena] ([id], [ime], [prezime], [mjesto_stanovanja]) 
+VALUES (0, N'Pero                                              ', 
+N'Peric', N'osijek                   ')
+
+SELECT * FROM #privremena
